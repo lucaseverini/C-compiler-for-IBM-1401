213ec26f2366057eced97971e2b02d1efafb9f39
Sean <sean.papay@gmail.com>
2014-09-10 10:11:03 -0700

    added a simple token class

---
+++ assignment2/Token.java
@@ -0,0 +1,30 @@
+package  wci.frontend.java;
+
+/* A class representing a single token of a java program
+ * based on the standards laid out in http://docs.oracle.com/javase/specs/jls/se7/html/jls-3.html
+ */
+
+public class Token {
+	public enum TokenType {
+		identifier, keyword, separator, operator,
+		integerLiteral, floatingPointLiteral, booleanLiteral,
+		characterLiteral, stringLiteral, nullLiteral
+	}
+	
+	private String text;
+	private TokenType type;
+	
+	protected Token(String text, TokenType type) {
+		this.text = text;
+		this.type = type;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public TokenType getType() {
+		return type;
+	}
+	
+}
