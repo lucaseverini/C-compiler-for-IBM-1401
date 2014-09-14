e954549acde407cddb9585932aa091b075cf7098
Sean <sean.papay@gmail.com>
2014-09-14 12:46:28 -0700

    added a tokenizer class that needs to be fleshed out

---
+++ assignment2/Tokenizer.java
@@ -0,0 +1,54 @@
+public class Tokenizer {
+	private String source;
+	private int position;
+	private int tokenStart;
+	
+	pubic Tokenizer(String source) {
+		this.source = source;
+		position = 0;
+		tokenStart = 0;
+	}
+	
+	private isSymbol(char c) {
+		return "(){}[];,.:+-*/|&^%<>!~?".contains(c)
+		
+	}
+	
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+	
+	private boolean isIdentifierStart(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '$' || c == '_';
+	}
+	
+	private boolean isIdentifierChar(char c) {
+		return isDigit(c) || isIdentifierStart(c);
+	}
+	
+	public Token nextToken() {
+		char c;
+		while(Character.isWhitespace(c = source.charAt(tokenStart))) {
+			tokenStaart++;
+		}
+		position = tokenStart;
+		if (isDigit(c)) {
+			return nextNumberLiteral();
+		}
+		if (isIdentifierStart(c)) {
+			return nextIdentOrKeyword();
+		}
+		if (isSymbol(c)) {
+			return nextSymbolOrComment();
+		}
+		if (c == '\'' || c == '\"') {
+			return nextStringOrCharLiteral();
+		}
+	}
+	
+
+	public void reset() {
+		position = 0;
+		tokenStart = 0;
+	}
+}
