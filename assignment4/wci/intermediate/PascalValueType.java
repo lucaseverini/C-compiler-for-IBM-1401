06c7ee43c323fcada6f7eb584baf4d33e66adeaa
Somebody <mpleva@SomebodyMBP.local>
2014-10-17 16:48:33 -0700

    Cleaning up after Sean
    Changed input.pas so that I can run the program.
    
    Changed CrossReferencer.java to include whether or not a name is a set
    
    Changed PascalValueType.java to go from a generic to a specific type
    instead of the other way around.

--- assignment4/wci/intermediate/PascalValueType.java
+++ assignment4/wci/intermediate/PascalValueType.java
@@ -3,17 +3,18 @@ package wci.intermediate;
 public class PascalValueType {
 	public PascalValueType() {
 	}
-	
+
 	public boolean equals(Object other) {
 		return other == this;
 	}
-	
+
 	//all the atomic types simply exist as singletons
 	//they have no defining properties apart from their uniqueness
 	//most compound types are built recursively from these, except enums
 	//and ranges, which are weird
-	public static PascalValueType REAL = new PascalValueType();
-	public static PascalIntegralType INTEGER = new PascalValueType();
-	public static PascalIntegralType CHAR = new PascalValueType();
-	public static PascalIntegralType BOOLEAN = new PascalValueType();
+	public static PascalValueType REAL = new PascalIntegralType();
+	public static PascalValueType INTEGER = new PascalIntegralType();
+	public static PascalValueType CHAR = new PascalIntegralType();
+	public static PascalValueType BOOLEAN = new PascalIntegralType();
+	public static PascalValueType SET = new PascalSetType();
 }
