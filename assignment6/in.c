df0941ba2db4b4a9415785fea97452ec8a115ceb
Somebody <mpleva@SomebodyMBP-3.local>
2014-11-08 20:53:27 -0800

    Scanner

--- assignment6/in.c
+++ assignment6/in.c
@@ -1,15 +1,27 @@
-int i = 5 * 6;
-int mod = 523871 % 15;
-int mod2 = 523871 % mod;
-//if ((((mod2 | 5) & 24L) >> 5) / 15.0) {
-if (mod == 5) {
-	what ++;
+// int i = 5 * 6;
+// int mod = 523871 % 15;
+// int mod2 = 523871 % mod;
+// //if ((((mod2 | 5) & 24L) >> 5) / 15.0) {
+// if (mod == 5) {
+// 	what ++;
+// }
+// /*int i = 0;
+// for(; i < 15; i++) {
+// 	who
+// }*/
+// while(i > 0) {
+// 	i --;
+// 	when += i;
+// }
+
+#include <stdio.h>
+
+int foo (a,b){
+	return sizeof(a);
 }
-/*int i = 0;
-for(; i < 15; i++) {
-	who
-}*/
-while(i > 0) {
-	i --;
-	when += i;
+
+int main() {
+	int i = 9;
+	printf("%d\n",foo(&i,2));
+	return 0;
 }
