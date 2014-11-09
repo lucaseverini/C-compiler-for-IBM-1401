d62baf8e6db1233f20dcd76f9158b645056a36df
Luca Severini <lucaseverini@mac.com>
2014-11-08 20:40:26 -0800

    Added test file for preprocessor preprocessor.c

---
+++ assignment6/preprocessor/preprocessor.c
@@ -0,0 +1,47 @@
+/* Multiline
+ * comment
+ */
+
+// another comment
+/*------*/
+
+// Include file
+#include "file.h"
+
+// Include system file
+#include <file.h>
+
+// Include file
+#include file.h
+
+// Define name
+#define DEBUG
+
+#ifdef DEBUG
+	#include "debug_file.h"
+#else
+	#include "nodebug_file.h"
+#endif	
+
+#ifndef DEBUG
+	#define DEBUG
+#endif
+
+#ifndef RELEASE
+	#define RELEASE
+#endif
+
+#ifdef RELEASE
+	#include release.h
+	#undef RELEASE
+#endif
+
+// Some assembly. Just ignore it
+#asm
+	MOV A,B
+	MOV M,A
+#endasm
+			
+#code  10000
+#data  20000
+#stack 10000
