3f0f59687cf2dfdc2a3bc1e780ab1b7a4019c310
Sean <sean.papay@gmail.com>
2014-11-20 23:38:07 -0800

    fixed in.c

--- scc1401/in.c
+++ scc1401/in.c
@@ -3,10 +3,11 @@ int fact(int n)
 	return n == 0 ? 1 : n * fact(n - 1);
 }
 
+int foo;
+
 int is_prime(int n)
 {
 	int i;
-	// <= one token or 2?
 	for (i = 2; i*i <= n; ++i) if (! n%i) return 0;
 	return 1;
 }
@@ -14,9 +15,9 @@ int is_prime(int n)
 int main(int argc, char *argv[])
 {
 	char str[] = "hfjkkjsfd";
+	int n = 0;
 	printf("%s",str);
 
-	int n = 0;
 	do {
 		switch(n % 3)
 		{
