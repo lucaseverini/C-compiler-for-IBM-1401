3031e4314dfe3dab83fd193bb520a7e347980928
Somebody <mpleva@SomebodyMBP-4.local>
2014-12-01 21:37:29 -0800

    Modified makefile to add required classes
    Added some packages

--- scc1401/src/retree/RetreeUtils.java
+++ scc1401/src/retree/RetreeUtils.java
@@ -1,3 +1,5 @@
+package retree;
+
 public class RetreeUtils {
 	public static String LBL_INS(String label, String operation, String ... args) {
 		String line = "     ";
@@ -11,14 +13,14 @@ public class RetreeUtils {
 		}
 		return line + "\n";
 	}
-	
+
 	public static String INS(String operation, String ... args) {
 		return LBL_INS("", operation, args);
 	}
-	
+
 	public static String CONST(int val) { return "$" + COD(val) + "$";}
 	public static String REG(String val) { return "R" + val;}
-	
+
 	public static String OFF(int offset) {
 		if (offset > 0) {
 			return "SP+" + COD(offset);
@@ -26,11 +28,11 @@ public class RetreeUtils {
 			return COD(-offset);
 		}
 	}
-	
+
 	public static String COD(int val) {
 		return val + "";
 		//todo
-		
+
 	}
 	public static String ADDR_REL(int base, int offset); {
 		return COD(base) + "+" + COD(offset);
