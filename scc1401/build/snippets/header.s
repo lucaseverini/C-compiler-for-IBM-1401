02a4128b250fb387ec5cae15007db1deed087b46
Luca Severini <lucaseverini@mac.com>
2014-12-13 13:19:02 -0800

    Added buid folder

---
+++ scc1401/build/snippets/header.s
@@ -0,0 +1,28 @@
+     ****************************************************************
+
+     READ      EQU  001                * Read area
+     PUNCH     EQU  101                * Punch area
+     PRINT     EQU  201                * Print area
+     
+     PRCPOS    DCW  000                * char position in print area
+     PUCPOS    DCW  000                * char position in punch area
+     PUNSIZ    DCW  @080@              * Size of punch area
+     PRTSIZ    DCW  @132@              * Size of print area
+     EOS       DCW  @'@                * End Of String char (string terminator)
+     EOL       DCW  @;@                * End Of Line char
+
+               ORG  87
+     X1        DSA  0                  * INDEX REGISTER 1
+               ORG  92
+     X2        DSA  0                  * INDEX REGISTER 2
+               ORG  97
+     X3        DSA  0                  * INDEX REGISTER 3
+
+               ORG  6000
+  
+     START     NOP
+     
+     ****************************************************************  
+     
+               SBR  X2, 400            * SET THE STACK
+               MCW  X2, X3
