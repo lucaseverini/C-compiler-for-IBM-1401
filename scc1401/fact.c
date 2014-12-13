3a0a016f160d4651ec32135b6ade104c86f5b21d
Luca Severini <lucaseverini@mac.com>
2014-12-13 13:15:32 -0800

    removed deletion of temp dir in makefile

--- scc1401/fact.c
+++ scc1401/fact.c
@@ -1,8 +1,10 @@
 int f = 1;
+int x = 0;
 
 int fact(int n)
 {
-	if (n) {
+	if (n)
+	{
 		f *= n;
 		fact(n-1);
 	}
@@ -10,7 +12,19 @@ int fact(int n)
 
 int main()
 {
-	int result = 0;
+	int result = 0, i;
+	char *prt = 201;
+	
+	for(i = 10; i ; i--)
+	{
+		prt[i] = 'A';
+	}
 	
 	result = fact(8);
+}
+
+int test(int v)
+{
+	x = v * 2;
+	return v;
 }
