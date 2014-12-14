9a83d9beed1be5c209f9fe0147dbd81fffd7018d
Sean Papay <sean.papay@gmail.com>
2014-12-14 00:52:16 -0800

    added hello.c

---
+++ scc1401/hello.c
@@ -0,0 +1,21 @@
+#include <nonstdlib.h>
+
+int main() {
+	char format_str[12]
+	format_str[0] = 'H';
+	format_str[1] = 'E';
+	format_str[2] = 'L';
+	format_str[3] = 'L';
+	format_str[4] = 'O';
+	format_str[5] = ' ';
+	format_str[6] = 'W';
+	format_str[7] = 'O';
+	format_str[8] = 'R';
+	format_str[9] = 'L';
+	format_str[10] = 'D';
+	format_str[11] = '\n';
+	format_str[12] = '\0';
+	
+	printf(format_str);
+
+}
