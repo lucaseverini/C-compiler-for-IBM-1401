0541dd2cde883ca437b1a0a58bc39c63771e24ed
Luca Severini <lucaseverini@mac.com>
2015-02-05 15:35:11 -0800

    Added some c source files

--- scc1401/hanoi.c
+++ scc1401/hanoi.c
@@ -1,31 +1,39 @@
-#include "stdio.h"
 
-void towers(int,char,char,char);
+#include <nonstdlib.h>
 
-void towers(int n,char frompeg,char topeg,char auxpeg)
+#define DISKS 10
+
+int towers (int n, char frompeg, char topeg, char auxpeg)
 {
-	if(n==1)
+	if(n == 1)
 	{ 
-		printf("\nMove disk 1 from peg %c to peg %c",frompeg,topeg);
+		printf("Move disk 1 from peg %c to peg %c\n", frompeg, topeg);
+		
 	    return;
 	}
 	  
 	/* Move top n-1 disks from A to B, using C as auxiliary */
-	towers(n-1,frompeg,auxpeg,topeg);
+	towers(n - 1, frompeg, auxpeg, topeg);
 	  
 	/* Move remaining disks from A to C */
-	printf("\nMove disk %d from peg %c to peg %c",n,frompeg,topeg);
+	printf("Move disk %d from peg %c to peg %c\n", n, frompeg, topeg);
 	
 	/* Move n-1 disks from B to C using A as auxiliary */
-	towers(n-1,auxpeg,topeg,frompeg);
+	towers(n - 1, auxpeg, topeg, frompeg);
+	
+	// If not specified a return of 0 is assumed
 }
 
-main ()
+int main ()
 { 
 	int n;
-	printf("Enter the number of disks : ");
-	scanf("%d",&n);
-	printf("The Tower of Hanoi involves the moves :\n\n");
-	towers(n,'A','C','B');
-	return 0;
+	n = (int)getchar();
+	printf("Number of disks : %d\n", n);
+	
+	printf("The Tower of Hanoi involves the moves :\n");
+	towers(n, 'A', 'C', 'B');
+	
+	printf("Done.\n");
+	
+	return 0; 	// Not necessary
 }
